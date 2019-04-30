35a35fdd4b0284033ddc35486110fb0b4d0487e7
Muhammed Fasal <48950721+muhammedfasal@users.noreply.github.com>
2019-04-30 23:08:29 +0530

    Add dataset from a single list of page

--- globaltrade/globaltrade/spiders/spider_1.py
+++ globaltrade/globaltrade/spiders/spider_1.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from urllib.parse import urljoin
 
 
 class Spider1Spider(scrapy.Spider):
     name = 'spider_1'
     allowed_domains = ['www.globaltrade.net']
     start_urls = [
-    'https://www.globaltrade.net/international-trade-import-exports/expert-service-provider-p/Fastfix-Inc.html'
+    'https://www.globaltrade.net/United-States/expert-service-provider.html'
     ]
 
     def parse(self, response):
-    	yield{
+    	urls = response.css('.profileNavigator::attr(href)').extract()
+    	for url in urls:
+    		url = response.urljoin(url)
+    		yield scrapy.Request(url=url, callback=self.parse_detials)
+
+    def parse_detials(self, response):
+    	dataset = {
     	# 'logo_url': response.css(".image .lazy::text").extract_first(),
     	'title': response.css(".sp-title span::text").extract_first(),
     	'sub_title': response.css(".sub::text").extract_first(),
@@ -19,6 +26,7 @@ class Spider1Spider(scrapy.Spider):
     	'about': response.css(".details p::text").getall(),
     	'website': response.css(".details a:nth-child(1)::text").extract_first(),
     	'languages_spoken': response.css("tr:nth-child(5) td+ td::text").extract_first(),
-    	# 'page_url': response.css(".sp-title span::text").extract_first(),
+    	'page_url': response.url,
     	}
+    	yield dataset
         
